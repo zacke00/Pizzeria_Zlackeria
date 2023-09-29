1c74657d8d35cffe7e80beccf3cb40c4a71b6d43
Zakaria Berglund <zacke.berglund@outlook.com>
2023-09-29 12:54:16 +0200

    go

--- pizzeria_zlackeria/src/Pages/Home/index.tsx
+++ pizzeria_zlackeria/src/Pages/Home/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const HomePage:React.FC = () => {
     return (
         <View>
-            <Text>Home</Text>
+            
         </View>
     );
 }
--- pizzeria_zlackeria/src/Routes/index.tsx
+++ pizzeria_zlackeria/src/Routes/index.tsx
@@ -2,6 +2,7 @@ import { BottomTabNavigationOptions, createBottomTabNavigator } from "@react-nav
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import HomePage from '../Pages/Home';
+import Assets from "../Assets";
 
 const Tab = createBottomTabNavigator();
 
@@ -9,12 +10,30 @@ const HomeRoutes: React.FC = () => {
     
         return (
             <Tab.Navigator initialRouteName="Start">
-                <Tab.Screen name="Home" component={HomePage} options={{}}/>
+                <Tab.Screen name="Home" component={HomePage} options={{...screenOptions, tabBarIcon: ({focused}) => <Assets.icons.Search style={iconStyle(focused)}/>}}/>
+      
                 <Tab.Screen name="Search" component={HomePage} options={{}}/>
                 <Tab.Screen name="Login" component={HomePage} options={{}}/>
             </Tab.Navigator>
         );
 };
+
+const iconStyle = ( focused: boolean ) => ({
+    opacity: focused ? 1 : 0.5,
+    marginBottom: focused ? 6 : 0,
+  });
+
+const screenOptions = {
+    headerShown: false,
+  
+    tabBarStyle: {
+      backgroundColor: "#000",
+      borderColor: "#000",
+      
+    },
+    tabBarActiveTintColor: "#f00",
+    tabBarInactiveTintColor: "#f0f",
+  } as BottomTabNavigationOptions;
 
 
 export default HomeRoutes;
